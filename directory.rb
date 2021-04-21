048a4c37550e51abdfb215843e690a2fff96ed0b
Jenny <jennybell42@gmail.com>
2021-04-21 14:54:11 +0100

    Directory finished up to exercises

--- directory.rb
+++ directory.rb
@@ -3,16 +3,15 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  @students = []
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty repeat this
   while !name.empty? do
     #add student hash to array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end 
 
@@ -34,7 +33,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -79,8 +78,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -88,4 +87,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
