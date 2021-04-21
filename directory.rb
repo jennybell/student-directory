bd475d1e35745bbb876d74ebfcde20ef7425e05d
Jenny <jennybell42@gmail.com>
2021-04-21 12:43:18 +0100

    Ability to save list added

--- directory.rb
+++ directory.rb
@@ -3,7 +3,6 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
   @students = []
   # get the first name
   name = gets.chomp
@@ -42,6 +41,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -51,6 +51,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -62,6 +64,16 @@ def show_students
   print_header
   print_student_list
   print_footer(@students)
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name]], [student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
